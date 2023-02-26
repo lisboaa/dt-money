654d7346ea5b5753b98f42658257faaca2f3e617
lisboaa <douglas.lisboa@edu.unipar.br>
2023-02-26 15:03:54 -0300

    Implementado componente Header; Implementado componente Summy;

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
+import { Summary } from './components/Summary';
 export function App() {
   return (
     <>
       <Header/>
+      <Summary/>
       <GlobalStyle/>
     </>
   );
